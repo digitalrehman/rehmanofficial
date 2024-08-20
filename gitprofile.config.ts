d7370aa3ce9dfac29c6121f81070a15fab73e50b
Abdul Rehman <145242032+digitalrehman@users.noreply.github.com>
2024-08-20 09:12:04 +0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -163,7 +163,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'night',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
